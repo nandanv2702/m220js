cf3f3d92e3183ea561d72761babcf0041382f03b
Nandan V <nandanv2702@gmail.com>
2022-01-02 21:20:49 +0530

    chapter 0

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import app from "./server"
+import { MongoClient } from "mongodb"
+import MoviesDAO from "../src/dao/moviesDAO"
+import UsersDAO from "./dao/usersDAO"
+import CommentsDAO from "./dao/commentsDAO"
+
+const port = process.env.PORT || 8000
+
+MongoClient.connect(
+  process.env.MFLIX_DB_URI,
+  { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    poolSize: 50, 
+    wtimeout: 2500,
+    connectTimeoutMS: 10000
+  },
+)
+  .catch(err => {
+    console.error(err.stack)
+    process.exit(1)
+  })
+  .then(async client => {
+    await MoviesDAO.injectDB(client)
+    await UsersDAO.injectDB(client)
+    await CommentsDAO.injectDB(client)
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`)
+    })
+  })
---
+++ test/config/mongoEnvironment.js
@@ -0,0 +1,22 @@
+const MongoClient = require("mongodb").MongoClient
+const NodeEnvironment = require("jest-environment-node")
+module.exports = class MongoEnvironment extends NodeEnvironment {
+  async setup() {
+    if (!this.global.mflixClient) {
+      this.global.mflixClient = await MongoClient.connect(
+        process.env.MFLIX_DB_URI,
+        { useNewUrlParser: true, useUnifiedTopology: true, poolSize: 50, wtimeout: 2500, connectTimeoutMS: 10000 }
+      )
+      await super.setup()
+    }
+  }
+
+  async teardown() {
+    await this.global.mflixClient.close()
+    await super.teardown()
+  }
+
+  runScript(script) {
+    return super.runScript(script)
+  }
+}
